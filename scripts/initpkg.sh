d34ca4c1a3b7db3aa603e2fe744ef71fa773d625
Druxorey <guillermogalavisg@gmail.com>
2024-02-02 21:49:54 -0400

    Added Redshift package and the script for the installation of the Yay package handler has been improved

--- scripts/initpkg.sh
+++ scripts/initpkg.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-packages_pacman=(xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector libinput timeshift blueman bluez-utils pulseaudio-bluetooth brightnessctl lsd scrot rofi cmus neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git bat btop speedtest-cli imagemagick exa tmux sxiv ncdu fzf cmatrix zip alsa-utils tumbler gvfs-smb samba samba-client hplip cups cups-pdf system-config-printerbas thunar nautilus dmenu vlc polybar libreoffice-fresh gimp jdk-openjdk flatpak pdfarranger steam libreoffice-fresh php)
+#* List of packages to install
 
-packages_yay=(visual-studio-code-bin notion-app-electron microsoft-edge-dev-bin brave-bin peaclock cava pipes.sh tetris-terminal-git minecraft-launcher)
+pacman_packages=(xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector libinput timeshift blueman bluez-utils pulseaudio-bluetooth brightnessctl lsd scrot rofi cmus neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git bat btop speedtest-cli imagemagick exa tmux sxiv ncdu fzf cmatrix zip alsa-utils tumbler gvfs-smb samba samba-client hplip cups cups-pdf system-config-printerbas thunar nautilus dmenu vlc polybar libreoffice-fresh gimp jdk-openjdk flatpak pdfarranger steam libreoffice-fresh php redshift)
+
+yay_packages=(visual-studio-code-bin notion-app-electron microsoft-edge-dev-bin brave-bin peaclock cava pipes.sh tetris-terminal-git minecraft-launcher)
+
+#* Instalation of the pacman packages
 
 echo -e "\e[1m !Wellcome to the bash installer!"
 
 echo -e "\e[33m Installing packages with pacman..."
-for package in ${packages_pacman[@]}; do
+for package in ${pacman_packages[@]}; do
     echo -e "\e[33m Installing $package..."
     sudo pacman -S --noconfirm $package
 done
 
-cd && git clone https://aur.archlinux.org/yay.git
-cd yay/ && makepkg -si
-cd .. && sudo rm -r yay/
+#* Instalation of yay
+
+echo "¿Do you want to install yay? (Y/n)"
+read -r output
+
+output=${output,,}
+
+if [[ -z $output ]]; then
+  output='y'
+fi
+
+if [[ $output == 'y' ]]; then
+    cd && git clone https://aur.archlinux.org/yay.git
+    cd yay/ && makepkg -si
+    cd .. && sudo rm -r yay
+else
+  echo "Yay  will not be installed"
+fi
+
+#* Instalation of yay packages
 
 echo -e "\e[33m Installing packages with yay..."
-for package in ${packages_yay[@]}; do
+for package in ${yay_packages[@]}; do
     echo "Installing $package..."
     yay -S --noconfirm $package
 done
+
+#* Enabling Services 
 
 echo -e "\e[33m Enabling Services..."
 

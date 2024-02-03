6208d31fc2dd8241b3ac033e5f5fc378ec4d119f
Druxorey <guillermogalavisg@gmail.com>
2024-02-02 23:25:39 -0400

    The code of the initpkg.sh file has been improved allowing interaction with the user and improvements in output.

--- scripts/initpkg.sh
+++ scripts/initpkg.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
+#* Colors for the prompts
+
+TITLE="\e[1;35m"
+RUNNING="\e[35m"
+QUESTION="\e[36m"
+SUCCESS="\e[1;32m"
+FAILED="\e[1;31m"
+END="\e[0m"
+LINE="\n"
+
 #* List of packages to install
 
 pacman_packages=(xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector libinput timeshift blueman bluez-utils pulseaudio-bluetooth brightnessctl lsd scrot rofi cmus neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git bat btop speedtest-cli imagemagick exa tmux sxiv ncdu fzf cmatrix zip alsa-utils tumbler gvfs-smb samba samba-client hplip cups cups-pdf system-config-printerbas thunar nautilus dmenu vlc polybar libreoffice-fresh gimp jdk-openjdk flatpak pdfarranger steam libreoffice-fresh php redshift)
 
 yay_packages=(visual-studio-code-bin notion-app-electron microsoft-edge-dev-bin brave-bin peaclock cava pipes.sh tetris-terminal-git minecraft-launcher)
 
+services=(cups)
+
 #* Instalation of the pacman packages
 
-echo -e "\e[1m !Wellcome to the bash installer!"
+echo -e "${LINE}${TITLE}Wellcome to the bash installer!${LINE}${END}"
+echo -e "${QUESTION}The installation of the Pacman packages will begin${END}"
 
-echo -e "\e[33m Installing packages with pacman..."
 for package in ${pacman_packages[@]}; do
-    echo -e "\e[33m Installing $package..."
+    echo -e "${RUNNING}Installing ${END}$package"
     sudo pacman -S --noconfirm $package
+
+    if [ $? -eq 0 ]
+    then
+    	echo -e "${SUCCESS}Installed the correctly${END}"
+    else
+    	echo -e "${FAILED}!!!ERROR ALERT!!! The following package could not be installed: ${END}$package"
+    fi
 done
 
 #* Instalation of yay
 
-echo "¿Do you want to install yay? (Y/n)"
+echo -e "${QUESTION}¿Do you want to install yay? (Y/n)${END}"
 read -r output
 
 output=${output,,}
@@ -32,19 +51,45 @@ if [[ $output == 'y' ]]; then
     cd yay/ && makepkg -si
     cd .. && sudo rm -r yay
 else
-  echo "Yay  will not be installed"
+	echo -e "${FAILED}Yay will not be installed${END}"
 fi
 
 #* Instalation of yay packages
 
-echo -e "\e[33m Installing packages with yay..."
+echo -e "${QUESTION}The installation of the Yay packages will begin${END}"
+
 for package in ${yay_packages[@]}; do
-    echo "Installing $package..."
+    echo -e "${RUNNING}Installing ${END}$package"
     yay -S --noconfirm $package
+
+    if [ $? -eq 0 ]
+    then
+    	echo -e "${SUCCESS}Installed the correctly${END}"
+    else
+    	echo -e "${FAILED}!!!ERROR ALERT!!! The following package could not be installed: ${END}$package"
+    fi
 done
 
 #* Enabling Services 
 
-echo -e "\e[33m Enabling Services..."
+echo -e "${QUESTION}¿Do you want to enable the services? (Y/n)${END}"
+read -r output
 
-sudo systemctl enable --now cups
+output=${output,,}
+
+if [[ -z $output ]]; then
+  output='y'
+fi
+
+if [[ $output == 'y' ]]; then
+
+	echo -e "${RUNNING}Enabling Services${END}"
+	for service in ${services[@]}; do
+		echo -e "${RUNNING}Enabling ${END}$service"
+		sudo systemctl enable --now $service
+	done
+fi
+	
+else
+  echo -e "${FAILED}!!WARNING!! The services are not enabled, if you need them please install and enable manually, we recommend enabling the services for a good experience${END}"
+fi

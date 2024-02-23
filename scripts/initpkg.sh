93f17423bdc26462fe1e1a616f9198c4a542341f
Druxorey <guillermogalavisg@gmail.com>
2024-02-23 13:30:04 -0400

    Fixed errors that prevented the script from executing correctly

--- scripts/initpkg.sh
+++ scripts/initpkg.sh
@@ -12,7 +12,7 @@ LINE="\n"
 
 #* List of packages to install
 
-pacman_packages=(xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings reflector libinput timeshift blueman bluez-utils pulseaudio-bluetooth brightnessctl lsd scrot rofi cmus neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git bat btop speedtest-cli imagemagick exa tmux sxiv ncdu fzf cmatrix zip alsa-utils tumbler gvfs-smb samba samba-client hplip cups cups-pdf system-config-printerbas thunar nautilus dmenu vlc polybar libreoffice-fresh gimp jdk-openjdk flatpak pdfarranger steam php redshift nodejs npm translate-shell xsel ripgrep fd the_silver_searcher)
+pacman_packages=(xorg bspwm sxhkd lxappearance picom nitrogen kitty lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings networkmanager reflector libinput timeshift blueman bluez-utils pulseaudio-bluetooth brightnessctl lsd scrot rofi cmus neovim gedit unzip p7zip unrar tar rsync htop exfat-utils fuse-exfat curl wget trash-cli ranger thefuck tldr gnome-keyring usbutils gthumb bash-completion neofetch vim git bat btop speedtest-cli imagemagick exa tmux sxiv ncdu fzf cmatrix zip alsa-utils tumbler gvfs-smb samba smbclient hplip cups cups-pdf system-config-printer thunar nautilus dmenu vlc polybar libreoffice-fresh gimp jdk-openjdk flatpak pdfarranger steam php redshift nodejs npm translate-shell xsel ripgrep fd the_silver_searcher)
 
 yay_packages=(visual-studio-code-bin notion-app-electron microsoft-edge-dev-bin brave-bin peaclock cava pipes.sh tetris-terminal-git minecraft-launcher gtypist)
 
@@ -27,8 +27,7 @@ for package in ${pacman_packages[@]}; do
     echo -e "${RUNNING}Installing ${END}$package"
     sudo pacman -S --noconfirm $package
 
-    if [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
     	echo -e "${SUCCESS}Installed the correctly${END}"
     else
     	echo -e "${FAILED}!!!ERROR ALERT!!! The following package could not be installed: ${END}$package"
@@ -62,8 +61,7 @@ for package in ${yay_packages[@]}; do
     echo -e "${RUNNING}Installing ${END}$package"
     yay -S --noconfirm $package
 
-    if [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
     	echo -e "${SUCCESS}Installed the correctly${END}"
     else
     	echo -e "${FAILED}!!!ERROR ALERT!!! The following package could not be installed: ${END}$package"
@@ -82,14 +80,11 @@ if [[ -z $output ]]; then
 fi
 
 if [[ $output == 'y' ]]; then
-
 	echo -e "${RUNNING}Enabling Services${END}"
 	for service in ${services[@]}; do
 		echo -e "${RUNNING}Enabling ${END}$service"
-		sudo systemctl enable --now $service
+		sudo systemctl enable $service
 	done
-fi
-	
 else
 	echo -e "${FAILED}!!WARNING!! The services are not enabled, if you need them please install and enable manually, we recommend enabling the services for a good experience${END}"
 fi
@@ -106,12 +101,9 @@ if [[ -z $output ]]; then
 fi
 
 if [[ $output == 'y' ]]; then
-
 	echo -e "${RUNNING}Installing zsh${END}"
 	sudo pacman -S zsh
     chsh -s /bin/zsh
-fi
-	
 else
     echo -e "${FAILED}!!WARNING!! The services are not enabled, if you need them please install and enable manually, we recommend enabling the services for a good experience${END}"
 fi

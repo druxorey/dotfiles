1b4817d3e668adc31d66f134dac338bda01ef128
Druxorey <guillermogalavisg@gmail.com>
2024-03-05 02:25:23 -0400

    Updated 'dotfiles-backup' file to support cmus configuration

---
+++ config/cmus/lib.pl
@@ -0,0 +1,398 @@
+/home/druxorey/Music/Música/7 A.M.flac
+/home/druxorey/Music/Música/Youngblood.flac
+/home/druxorey/Music/Música/Bang!.flac
+/home/druxorey/Music/Música/Rueda De Prensa.flac
+/home/druxorey/Music/Música/La Vida Es Un Freestyle.flac
+/home/druxorey/Music/Música/Haters.flac
+/home/druxorey/Music/Música/Réplica.flac
+/home/druxorey/Music/Música/Próceres.flac
+/home/druxorey/Music/Música/Pa  ganá.flac
+/home/druxorey/Music/Música/Bendecidos.flac
+/home/druxorey/Music/Música/Génesis.flac
+/home/druxorey/Music/Música/G.O.A.T.flac
+/home/druxorey/Music/Música/Bottas.flac
+/home/druxorey/Music/Música/ARCANGEL - BZRP Music Sessions #54.flac
+/home/druxorey/Music/Música/Teca.flac
+/home/druxorey/Music/Música/The Nights.flac
+/home/druxorey/Music/Música/The Days.flac
+/home/druxorey/Music/Música/Sonido Nativo del Río.flac
+/home/druxorey/Music/Música/She Don't Give A Fo.flac
+/home/druxorey/Music/Música/ORO SHAMPEIN.flac
+/home/druxorey/Music/Música/AEROBICO REMIX.flac
+/home/druxorey/Music/Música/OCASO.flac
+/home/druxorey/Music/Música/BIG SOTO - BZRP Music Sessions #28.flac
+/home/druxorey/Music/Música/bad guy.flac
+/home/druxorey/Music/Música/everything i wanted.flac
+/home/druxorey/Music/Música/Un Flow de Infarto.flac
+/home/druxorey/Music/Música/NICKI NICOLE - BZRP Music Sessions #13.flac
+/home/druxorey/Music/Música/Cazzu - BZRP Music Sessions #32.flac
+/home/druxorey/Music/Música/YaMeFui.flac
+/home/druxorey/Music/Música/Toy en el Mic.flac
+/home/druxorey/Music/Música/No soy Eterno.flac
+/home/druxorey/Music/Música/Fruto.flac
+/home/druxorey/Music/Música/Penas de Antaño.flac
+/home/druxorey/Music/Música/Toxic.flac
+/home/druxorey/Music/Música/Muerte En Hawaii.flac
+/home/druxorey/Music/Música/Na.flac
+/home/druxorey/Music/Música/Ni de Oro Ni de Goldfield.mp3
+/home/druxorey/Music/Música/Canbiate.mp3
+/home/druxorey/Music/Música/El Mundo Ya Se Acabó.mp3
+/home/druxorey/Music/Música/Canción de la Prisión.mp3
+/home/druxorey/Music/Música/Can You Hear It.mp3
+/home/druxorey/Music/Música/Guía para la Acción.mp3
+/home/druxorey/Music/Música/Canpesino.mp3
+/home/druxorey/Music/Música/Rialaif.mp3
+/home/druxorey/Music/Música/Así Mismo.mp3
+/home/druxorey/Music/Música/Corazones de Piedra.mp3
+/home/druxorey/Music/Música/La Voz.mp3
+/home/druxorey/Music/Música/Visiones.mp3
+/home/druxorey/Music/Música/Advertencia.mp3
+/home/druxorey/Music/Música/De la Vida Como Pelicula, Tragedia, Comedia y Ficcion.flac
+/home/druxorey/Music/Música/C'est La Mort.flac
+/home/druxorey/Music/Música/Es Épico.flac
+/home/druxorey/Music/Música/Ser Vero.flac
+/home/druxorey/Music/Música/En el Valle de las Sombras.flac
+/home/druxorey/Music/Música/Maquiavélico.flac
+/home/druxorey/Music/Música/Mundo de Piedra.flac
+/home/druxorey/Music/Música/Sin Mercy.flac
+/home/druxorey/Music/Música/Un Día en el Barrio.flac
+/home/druxorey/Music/Música/Llovía.flac
+/home/druxorey/Music/Música/Y en un Espejo Vi.flac
+/home/druxorey/Music/Música/La Hora del Juicio.flac
+/home/druxorey/Music/Música/De Mi Muerte.flac
+/home/druxorey/Music/Música/Jeremías 17-5.flac
+/home/druxorey/Music/Música/Mucho Gusto.flac
+/home/druxorey/Music/Música/Clima Tropical.flac
+/home/druxorey/Music/Música/Vida.flac
+/home/druxorey/Music/Música/Aceptas.flac
+/home/druxorey/Music/Música/Martillos y Ruedas.flac
+/home/druxorey/Music/Música/Hace Falta Soñar.flac
+/home/druxorey/Music/Música/Americanos.flac
+/home/druxorey/Music/Música/Pensando en Ti.flac
+/home/druxorey/Music/Música/Y la Felicidad Qué.flac
+/home/druxorey/Music/Música/No Justice.flac
+/home/druxorey/Music/Música/Únetenos.flac
+/home/druxorey/Music/Música/Quién Eres.flac
+/home/druxorey/Music/Música/Perdiendo la Fe.flac
+/home/druxorey/Music/Música/Ley del Hielo.flac
+/home/druxorey/Music/Música/Epílogo.flac
+/home/druxorey/Music/Música/Y la Felicidad Que.flac
+/home/druxorey/Music/Música/Tripolar.flac
+/home/druxorey/Music/Música/Buenas Noches.flac
+/home/druxorey/Music/Música/Darealhipapitis.mp3
+/home/druxorey/Music/Música/Is Canzoo.mp3
+/home/druxorey/Music/Música/Les Guste o No.mp3
+/home/druxorey/Music/Música/Índigo.mp3
+/home/druxorey/Music/Música/Zoo.mp3
+/home/druxorey/Music/Música/Cuando Vayas Conmigo.mp3
+/home/druxorey/Music/Música/No Pueden Llamarse Muertos.mp3
+/home/druxorey/Music/Música/Mañana Será Otro Día.mp3
+/home/druxorey/Music/Música/On Datop.mp3
+/home/druxorey/Music/Música/Monte y Culebra.mp3
+/home/druxorey/Music/Música/A Dónde Se Fue la Conciencia.mp3
+/home/druxorey/Music/Música/Epílogo.mp3
+/home/druxorey/Music/Música/El Primer Trago.flac
+/home/druxorey/Music/Música/Sigo Siendo el Rey.flac
+/home/druxorey/Music/Música/Visto a las 0000.flac
+/home/druxorey/Music/Música/Mucha Data.flac
+/home/druxorey/Music/Música/Rally.flac
+/home/druxorey/Music/Música/Penas y Problemas.flac
+/home/druxorey/Music/Música/Mentiste.flac
+/home/druxorey/Music/Música/Al Revés.flac
+/home/druxorey/Music/Música/Ya No Quiero.flac
+/home/druxorey/Music/Música/Fuego.flac
+/home/druxorey/Music/Música/Nada.flac
+/home/druxorey/Music/Música/La Clase.flac
+/home/druxorey/Music/Música/Attention.flac
+/home/druxorey/Music/Música/CHUCKY73 - BZRP Music Sessions #43.flac
+/home/druxorey/Music/Música/Dos Cajas de Globos.flac
+/home/druxorey/Music/Música/Silbando.flac
+/home/druxorey/Music/Música/Una de Dos.flac
+/home/druxorey/Music/Música/Jordan.flac
+/home/druxorey/Music/Música/Modo Sexo.flac
+/home/druxorey/Music/Música/WOW.flac
+/home/druxorey/Music/Música/A Mi Lado.flac
+/home/druxorey/Music/Música/Nadie Como Tú.flac
+/home/druxorey/Music/Música/Pura Vida.flac
+/home/druxorey/Music/Música/Llorare por Ti.flac
+/home/druxorey/Music/Música/Hollows.flac
+/home/druxorey/Music/Música/Hijos del Coraje.flac
+/home/druxorey/Music/Música/Como Quererte.flac
+/home/druxorey/Music/Música/Skywalker.flac
+/home/druxorey/Music/Música/Roto por Dentro.flac
+/home/druxorey/Music/Música/Inferno.flac
+/home/druxorey/Music/Música/Qué Bien Se Siente.flac
+/home/druxorey/Music/Música/Tommy Shelby.flac
+/home/druxorey/Music/Música/Tirando Flow Sesh #10.flac
+/home/druxorey/Music/Música/DANI RIBA - BZRP Music Sessions #24.flac
+/home/druxorey/Music/Música/Mi Estilo.flac
+/home/druxorey/Music/Música/Antes que sea tarde.flac
+/home/druxorey/Music/Música/Detrás de lo Caro.flac
+/home/druxorey/Music/Música/Solo Una Vez.flac
+/home/druxorey/Music/Música/Lo Olvidaste.flac
+/home/druxorey/Music/Música/La Vibe.flac
+/home/druxorey/Music/Música/01 dE ENEro.flac
+/home/druxorey/Music/Música/Hello Cotto.flac
+/home/druxorey/Music/Música/Rockstar.flac
+/home/druxorey/Music/Música/Hijo de la Noche.flac
+/home/druxorey/Music/Música/Fvck Luv.flac
+/home/druxorey/Music/Música/Goteo.flac
+/home/druxorey/Music/Música/A Punta de Espada.flac
+/home/druxorey/Music/Música/Te Traje Flores.flac
+/home/druxorey/Music/Música/Hitboy.flac
+/home/druxorey/Music/Música/H.I.E.L.O..flac
+/home/druxorey/Music/Música/Sold Out Dates.flac
+/home/druxorey/Music/Música/Chico Estrella.flac
+/home/druxorey/Music/Música/Sudor y Trabajo.flac
+/home/druxorey/Music/Música/Pintao.flac
+/home/druxorey/Music/Música/Cuanto.flac
+/home/druxorey/Music/Música/Rapido.flac
+/home/druxorey/Music/Música/Malbec.flac
+/home/druxorey/Music/Música/Fifty Fifty.flac
+/home/druxorey/Music/Música/Valentino.flac
+/home/druxorey/Music/Música/Cascada.flac
+/home/druxorey/Music/Música/Ella Es Mi Bitch.flac
+/home/druxorey/Music/Música/Si Quieren Frontear.flac
+/home/druxorey/Music/Música/GIVENCHY.flac
+/home/druxorey/Music/Música/Celosa.flac
+/home/druxorey/Music/Música/Antes de Perderte.flac
+/home/druxorey/Music/Música/DUKI - BZRP Music Sessions #50.flac
+/home/druxorey/Music/Música/Si Me Sobrara el Tiempo.flac
+/home/druxorey/Music/Música/JEFES DEL SUDOESTE.flac
+/home/druxorey/Music/Música/RoCKSTAR 2.0.flac
+/home/druxorey/Music/Música/hARAkiRI.flac
+/home/druxorey/Music/Música/cONTRA  Mi.flac
+/home/druxorey/Music/Música/CSIpher.flac
+/home/druxorey/Music/Música/GiGi.flac
+/home/druxorey/Music/Música/don t liE.flac
+/home/druxorey/Music/Música/antes de perderte (OG VERSION).flac
+/home/druxorey/Music/Música/TROYA.flac
+/home/druxorey/Music/Música/uNO dOS.flac
+/home/druxorey/Music/Música/N.C.L.C.flac
+/home/druxorey/Music/Música/aPoLLo13.flac
+/home/druxorey/Music/Música/Ultimo Tren a Ameri.flac
+/home/druxorey/Music/Música/Remember Me.flac
+/home/druxorey/Music/Música/The Real Slim Shady.flac
+/home/druxorey/Music/Música/Lose Yourself.flac
+/home/druxorey/Music/Música/Rap God.flac
+/home/druxorey/Music/Música/Killshot.flac
+/home/druxorey/Music/Música/Godzilla.flac
+/home/druxorey/Music/Música/Gansta's Paradise.flac
+/home/druxorey/Music/Música/Modo On.flac
+/home/druxorey/Music/Música/Blood & Water.flac
+/home/druxorey/Music/Música/D.O.M.I.N.G.O.flac
+/home/druxorey/Music/Música/HOMER EL MERO MERO - BZRP Music Sessions #30.flac
+/home/druxorey/Music/Música/Ciclo.flac
+/home/druxorey/Music/Música/Tierra.flac
+/home/druxorey/Music/Música/Super Jon-Z.flac
+/home/druxorey/Music/Música/Yoidoreshirazu.flac
+/home/druxorey/Music/Música/QUEEN.flac
+/home/druxorey/Music/Música/Rendezvous.flac
+/home/druxorey/Music/Música/Demon Lord.flac
+/home/druxorey/Music/Música/El Arte Sano.flac
+/home/druxorey/Music/Música/Relleno.flac
+/home/druxorey/Music/Música/KHEA - BZRP Music Sessions #34.flac
+/home/druxorey/Music/Música/Tengo 30.flac
+/home/druxorey/Music/Música/L-GANTE - BZRP Music Sessions #38.flac
+/home/druxorey/Music/Música/Ímpetu.flac
+/home/druxorey/Music/Música/La Respuesta.flac
+/home/druxorey/Music/Música/Apaga el celular.flac
+/home/druxorey/Music/Música/Bufón.flac
+/home/druxorey/Music/Música/Si Te Vas.flac
+/home/druxorey/Music/Música/Eclipse.flac
+/home/druxorey/Music/Música/Amor Ciego.flac
+/home/druxorey/Music/Música/Wake Me.flac
+/home/druxorey/Music/Música/CHANGE.flac
+/home/druxorey/Music/Música/California.flac
+/home/druxorey/Music/Música/La Trampa es Ley.flac
+/home/druxorey/Music/Música/Los del Espacio.flac
+/home/druxorey/Music/Música/Samurai.flac
+/home/druxorey/Music/Música/Se Terminó.flac
+/home/druxorey/Music/Música/Tussi.flac
+/home/druxorey/Music/Música/Cazame.flac
+/home/druxorey/Music/Música/MHD - BZRP Music Sessions #44.flac
+/home/druxorey/Music/Música/DISPARA.flac
+/home/druxorey/Music/Música/El Bolero.flac
+/home/druxorey/Music/Música/Tu Paz.flac
+/home/druxorey/Music/Música/1708.flac
+/home/druxorey/Music/Música/Milagrosa.flac
+/home/druxorey/Music/Música/MORNING.flac
+/home/druxorey/Music/Música/AL BORDE.flac
+/home/druxorey/Music/Música/TOI HECHO.flac
+/home/druxorey/Music/Música/Rincón.flac
+/home/druxorey/Music/Música/MILO J - BZRP Music Sessions #57.flac
+/home/druxorey/Music/Música/TU MANTA.flac
+/home/druxorey/Music/Música/M.A.I.flac
+/home/druxorey/Music/Música/DESEO PERDER.flac
+/home/druxorey/Music/Música/SINCERA TE.flac
+/home/druxorey/Music/Música/A1RE.flac
+/home/druxorey/Music/Música/ALUMBRE.flac
+/home/druxorey/Music/Música/UNA BALA.flac
+/home/druxorey/Music/Música/TE FUI A SEGUIR.flac
+/home/druxorey/Music/Música/CARENCIAS DE CORDURA.flac
+/home/druxorey/Music/Música/TRAP.N.EXPORT.flac
+/home/druxorey/Music/Música/Manifiesto.flac
+/home/druxorey/Music/Música/I’m Your Treasure Box.flac
+/home/druxorey/Music/Música/NATHY PELUSO - BZRP Music Sessions #36.flac
+/home/druxorey/Music/Música/Akapelinho.flac
+/home/druxorey/Music/Música/Guapo.flac
+/home/druxorey/Music/Música/Quavo.flac
+/home/druxorey/Music/Música/Tumbando el Club.flac
+/home/druxorey/Music/Música/NEUTRO SHORTY - BZRP Music Sessions #21.flac
+/home/druxorey/Music/Música/Soy Yo.flac
+/home/druxorey/Music/Música/Intro I.flac
+/home/druxorey/Music/Música/Paralyzed.flac
+/home/druxorey/Music/Música/Intro II.flac
+/home/druxorey/Music/Música/Intro III.flac
+/home/druxorey/Music/Música/Outro.flac
+/home/druxorey/Music/Música/The Search.flac
+/home/druxorey/Music/Música/When I Grow Up.flac
+/home/druxorey/Music/Música/CLOUDS.flac
+/home/druxorey/Music/Música/Shorty.flac
+/home/druxorey/Music/Música/CAEN LAS ESTRELLAS.flac
+/home/druxorey/Music/Música/NICKY JAM - BZRP Music Sessions #41.flac
+/home/druxorey/Music/Música/PAULO LONDRA - BZRP Music Sessions #23.flac
+/home/druxorey/Music/Música/Party en el Barrio.flac
+/home/druxorey/Music/Música/Hay Siempre un Sentimiento Muerto en un Corazón Roto.flac
+/home/druxorey/Music/Música/PTAZETA - BZRP Music Sessions #45.flac
+/home/druxorey/Music/Música/Dream lantern.flac
+/home/druxorey/Music/Música/Zenzenzense.flac
+/home/druxorey/Music/Música/Sparkle.flac
+/home/druxorey/Music/Música/Nandemonaiya.flac
+/home/druxorey/Music/Música/Voice Of Wind.flac
+/home/druxorey/Music/Música/Celebration.flac
+/home/druxorey/Music/Música/Grand Escape.flac
+/home/druxorey/Music/Música/We'll Be Alright.flac
+/home/druxorey/Music/Música/Is There Still Anything That Love Can Do.flac
+/home/druxorey/Music/Música/KANATA HALUKA.flac
+/home/druxorey/Music/Música/Suzume.flac
+/home/druxorey/Music/Música/Tamaki.flac
+/home/druxorey/Music/Música/Tears of Suzume.flac
+/home/druxorey/Music/Música/Me Lo Pienso.flac
+/home/druxorey/Music/Música/Sin Berretín.flac
+/home/druxorey/Music/Música/La Cátedra.flac
+/home/druxorey/Music/Música/Rap Bruto.flac
+/home/druxorey/Music/Música/Pecador.flac
+/home/druxorey/Music/Música/René.flac
+/home/druxorey/Music/Música/RESIDENTE - BZRP Music Sessions #49.flac
+/home/druxorey/Music/Música/This is Not America.flac
+/home/druxorey/Music/Música/PING PONG.flac
+/home/druxorey/Music/Música/Nastinasti.flac
+/home/druxorey/Music/Música/SNOW THA PRODUCT - BZRP Music Sessions #39.flac
+/home/druxorey/Music/Música/En La Trampa.flac
+/home/druxorey/Music/Música/ESTA QUE QUEMA.flac
+/home/druxorey/Music/Música/Infinito al Doble.flac
+/home/druxorey/Music/Música/NO PARA.flac
+/home/druxorey/Music/Música/Surfeando el Tsunami.flac
+/home/druxorey/Music/Música/TRAP DE VERDAD.flac
+/home/druxorey/Music/Música/ORO Y PLATINO.flac
+/home/druxorey/Music/Música/INOLVIDABLE.flac
+/home/druxorey/Music/Música/SEA LA HORA QUE SEA.flac
+/home/druxorey/Music/Música/DE CASA EN CASA.flac
+/home/druxorey/Music/Música/TRAP ON THE ROCKS.flac
+/home/druxorey/Music/Música/TODO AUTOMATICO.flac
+/home/druxorey/Music/Música/INSPIRACION DIVINA.flac
+/home/druxorey/Music/Música/NO HOOK.flac
+/home/druxorey/Music/Música/EN EL INFIERNO NO LOS VI.flac
+/home/druxorey/Music/Música/QUE DESVELO.flac
+/home/druxorey/Music/Música/CUANDO FUE.flac
+/home/druxorey/Music/Música/ACA NO SE SALVA NINGUNO.flac
+/home/druxorey/Music/Música/MI AMIGA LA ASESINA.flac
+/home/druxorey/Music/Música/EN LA JUNGLA.flac
+/home/druxorey/Music/Música/OTRA NOCHE.flac
+/home/druxorey/Music/Música/COMO LO COME.flac
+/home/druxorey/Music/Música/SETEADO.flac
+/home/druxorey/Music/Música/UNA DOSIS.flac
+/home/druxorey/Music/Música/POR QUE.flac
+/home/druxorey/Music/Música/ACELERA 2.flac
+/home/druxorey/Music/Música/DORMIR ES PARA GILES.flac
+/home/druxorey/Music/Música/RELOJES REVENTADOS.flac
+/home/druxorey/Music/Música/UOH OH OH.flac
+/home/druxorey/Music/Música/CUÁNTOS TÉRMINOS.flac
+/home/druxorey/Music/Música/CUÁNTO VALE HACER EL AMOR.flac
+/home/druxorey/Music/Música/A POR TODO.flac
+/home/druxorey/Music/Música/CUÁL.flac
+/home/druxorey/Music/Música/PA ESTA SOLEDAD.flac
+/home/druxorey/Music/Música/HERIDAS AL FUEGO.flac
+/home/druxorey/Music/Música/TODO PAGO.flac
+/home/druxorey/Music/Música/CÓMO CHILLA ELLA.flac
+/home/druxorey/Music/Música/UN PISO MÁS.flac
+/home/druxorey/Music/Música/SER EL TRAP.flac
+/home/druxorey/Music/Música/Rara Vez.flac
+/home/druxorey/Music/Música/Five Nights at Freddy s.flac
+/home/druxorey/Music/Música/Sola.flac
+/home/druxorey/Music/Música/Házmelo.flac
+/home/druxorey/Music/Música/Tiago pzk - Entre Nosotros.flac
+/home/druxorey/Music/Música/TIAGO PZK - BZRP Music Sessions #48.flac
+/home/druxorey/Music/Música/Hopes And Dreams.flac
+/home/druxorey/Music/Música/SAVE The World.flac
+/home/druxorey/Music/Música/Battle Against A True Hero.flac
+/home/druxorey/Music/Música/Blood.flac
+/home/druxorey/Music/Música/TRUENO - BZRP Music Sessions #6.flac
+/home/druxorey/Music/Música/PANAMÁ.flac
+/home/druxorey/Music/Música/Stressed Out.flac
+/home/druxorey/Music/Música/Heathens.flac
+/home/druxorey/Music/Música/The remedy for a broken heart.flac
+/home/druxorey/Music/Música/LA PRIMERA VEZ.flac
+/home/druxorey/Music/Música/De la Ciudad del Tango.flac
+/home/druxorey/Music/Música/SACARTE TO'.flac
+/home/druxorey/Music/Música/AL LIMITE.flac
+/home/druxorey/Music/Música/Nati.flac
+/home/druxorey/Music/Música/Pastel Con Nutella.flac
+/home/druxorey/Music/Música/Pasa Que Yo.flac
+/home/druxorey/Music/Música/Salgo a Cazar.flac
+/home/druxorey/Music/Música/Casi un G.flac
+/home/druxorey/Music/Música/Vamo a Darle.flac
+/home/druxorey/Music/Música/Hidro.flac
+/home/druxorey/Music/Música/Vuelta a la Luna.flac
+/home/druxorey/Music/Música/El Amanecer.flac
+/home/druxorey/Music/Música/Enamorada del Diablo.flac
+/home/druxorey/Music/Música/Linaje.flac
+/home/druxorey/Music/Música/Tragos Fuertes.flac
+/home/druxorey/Music/Música/Muevelo.flac
+/home/druxorey/Music/Música/No Se.flac
+/home/druxorey/Music/Música/Tamo Loco.flac
+/home/druxorey/Music/Música/DESFILAR MIS PENAS.flac
+/home/druxorey/Music/Música/BARDOS Y VERDADES.flac
+/home/druxorey/Music/Música/COMO TIENE QUE SER.flac
+/home/druxorey/Music/Música/CALOR.flac
+/home/druxorey/Music/Música/FULL ICE.flac
+/home/druxorey/Music/Música/NEGOCIANDO POR TIEMPO.flac
+/home/druxorey/Music/Música/BUENOS AIRES ES AMOR.flac
+/home/druxorey/Music/Música/DONDE LAS LUCES.flac
+/home/druxorey/Music/Música/TANTAS TENTACIONES.flac
+/home/druxorey/Music/Música/ALMA.flac
+/home/druxorey/Music/Música/PARA SACARMELO.flac
+/home/druxorey/Music/Música/Todo de Oro.flac
+/home/druxorey/Music/Música/Traje Unos Tangos.flac
+/home/druxorey/Music/Música/Quiere Comer.flac
+/home/druxorey/Music/Música/Sin Conexión.flac
+/home/druxorey/Music/Música/TARDE.flac
+/home/druxorey/Music/Música/SOBRE EL AGUA.flac
+/home/druxorey/Music/Música/PERDAMONO.flac
+/home/druxorey/Music/Música/MORDIENDO EL BOZAL.flac
+/home/druxorey/Music/Música/YSY A - BZRP Music Sessions #37.flac
+/home/druxorey/Music/Música/Meteoritos Vs Dinosaurios.flac
+/home/druxorey/Music/Música/Una Luz en Todo Esto.flac
+/home/druxorey/Music/Música/SUBIDÓN.flac
+/home/druxorey/Music/Música/QUIEREN MÁS.flac
+/home/druxorey/Music/Música/MIS PECADOS.flac
+/home/druxorey/Music/Música/SIEMPRE LO MISMO.flac
+/home/druxorey/Music/Música/CUANTAS VECES.flac
+/home/druxorey/Music/Música/DE UNA.flac
+/home/druxorey/Music/Música/MUEREN X MATARME.flac
+/home/druxorey/Music/Música/TUUUYO.flac
+/home/druxorey/Music/Música/FLECHAZO EN EL CENTRO.flac
+/home/druxorey/Music/Música/MI CIUDAD.flac
+/home/druxorey/Music/Música/SONIDO DEL AÑO.flac
+/home/druxorey/Music/Música/TODA LA VIDA.flac
+/home/druxorey/Music/Música/LUGARES QUE LLEGO.flac
+/home/druxorey/Music/Música/+ QUE LA DROGA.flac
+/home/druxorey/Music/Música/NO DA MÁS.flac
+/home/druxorey/Music/Música/COPENHAGUE.flac
+/home/druxorey/Music/Música/24-7 6.5.flac
+/home/druxorey/Music/Música/NUNCA ENTENDEMO.flac
+/home/druxorey/Music/Música/NO NEGOCIO CON MI ALMA.flac
+/home/druxorey/Music/Música/LOCO X TU CUERPO.flac
+/home/druxorey/Music/Música/ASESINO.flac
+/home/druxorey/Music/Música/GANAS.flac

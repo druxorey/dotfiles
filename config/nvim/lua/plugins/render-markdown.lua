826b75650b03a33cfd3633aafdfb79b427ca6467
Druxorey <druxorey@gmail.com>
2024-08-09 01:42:13 -0400

    feat(config): 'render-markdown.vim' plugin.

---
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,5 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+}

b5bf6aeab1147dd858000f17586fa81ba507b026
Druxorey <druxorey@gmail.com>
2024-08-10 00:36:21 -0400

    fix(config): deleted unnecessary comments.

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,8 +1,12 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim" -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    keys = {
+        {"<leader>E", false},
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim"
     },
     opts = {
         window = {
--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,5 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
 }

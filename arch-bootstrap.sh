a2d97e2540a59372f669c2645cd4c522e305253e
Druxorey <druxorey@gmail.com>
2024-08-15 21:51:31 -0400

    feat(arch-bootstrap): separate script from package list.

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -10,18 +10,7 @@ LINE="\n"
 
 isAutomated=0
 
-basePacmanList=(bspwm git kitty libinput lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance networkmanager nitrogen picom polybar reflector sxhkd timeshift ufw xorg)
-
-utilsPacmanList=(bash-completion bat btop cmatrix cmus curl exfat-utils eza fastfetch ffmpeg fd flatpak fuse-exfat fzf github-cli gnome-keyring hugo imagemagick ncdu neovim nodejs npm p7zip pandoc-cli php pv ranger redshift ripgrep rofi rsync rust scrot silicon sl speedtest-cli sxiv tar the_silver_searcher thefuck tldr tmux tokei translate-shell trash-cli unrar unzip usbutils vim viu wget xclip xsel yt-dlp zip zoxide zathura zathura-pdf-mupdf)
-
-driversPacmanList=(alsa-utils blueman bluez-utils brightnessctl cups cups-pdf ffmpegthumbnailer gestures gvfs-mtp gvfs-smb hplip jdk-openjdk linux-headers mtpfs noto-fonts noto-fonts-cjk noto-fonts-emoji ntfs-3g papirus-icon-theme pulseaudio-bluetooth python-pip python-setuptools python-virtualenv samba simple-scan smbclient system-config-printer tlp tumbler) 
-
-guiPacmanList=(code discord gedit gimp gloobus-preview gthumb libreoffice-fresh nautilus obs-studio obsidian sigil steam thunar vbam-wx virtualbox vlc)
-
-yayPackagesList=(ani-cli brave-bin cava googler gtypist manga-cli-git microsoft-edge-dev-bin minecraft-launcher notion-app-electron oh-my-posh peaclock pipes.sh tetris-terminal-git tumbler-extra-thumbnailers)
-
-servicesList=(ufw.service cups NetworkManager lightdm bluetooth.service)
-
+source "$(dirname "$0")/arch-packages.sh"
 
 function help() {
     echo
@@ -86,7 +75,7 @@ function enableService() {
 function copyDotfiles() {
     local dotfilesPath="6PYqO7W9oEQJb04nSoqajW26Xexnbw3z/"
     local gitClone="git clone https://github.com/druxorey/dotfiles.git $dotfilesPath"
-    
+
     if $gitClone; then
         sudo pacman -S --noconfirm rsync
         rsync -av --delete $dotfilesPath/config/* $HOME/.config/
---
+++ arch-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+basePacmanList=(bspwm git kitty libinput lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance networkmanager nitrogen picom polybar reflector sxhkd timeshift ufw xorg)
+
+utilsPacmanList=(bash-completion bat btop cmatrix cmus curl exfat-utils eza fastfetch ffmpeg fd flatpak fuse-exfat fzf github-cli gnome-keyring hugo imagemagick ncdu neovim nodejs npm p7zip pandoc-cli php pv ranger redshift ripgrep rofi rsync rust scrot silicon sl speedtest-cli sxiv tar the_silver_searcher thefuck tldr tmux tokei translate-shell trash-cli unrar unzip usbutils vim viu wget xclip xsel yt-dlp zip zoxide zathura zathura-pdf-mupdf)
+
+driversPacmanList=(alsa-utils blueman bluez-utils brightnessctl cups cups-pdf ffmpegthumbnailer gestures gvfs-mtp gvfs-smb hplip jdk-openjdk linux-headers mtpfs noto-fonts noto-fonts-cjk noto-fonts-emoji ntfs-3g papirus-icon-theme pulseaudio-bluetooth python-pip python-setuptools python-virtualenv samba simple-scan smbclient system-config-printer tlp tumbler) 
+
+guiPacmanList=(code discord gedit gimp gloobus-preview gthumb libreoffice-fresh nautilus obs-studio obsidian sigil steam thunar vbam-wx virtualbox vlc)
+
+yayPackagesList=(ani-cli brave-bin cava googler gtypist manga-cli-git microsoft-edge-dev-bin minecraft-launcher notion-app-electron oh-my-posh peaclock pipes.sh tetris-terminal-git tumbler-extra-thumbnailers)
+
+servicesList=(ufw.service cups NetworkManager lightdm bluetooth.service)

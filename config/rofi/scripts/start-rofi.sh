c117d7fba2d107baa55b8e5a40cb276f96a250df
Druxorey <druxorey@gmail.com>
2024-08-20 21:26:25 -0400

    feat(config): implemented rofi-calc.

--- config/rofi/scripts/start-rofi.sh
+++ config/rofi/scripts/start-rofi.sh
@@ -4,12 +4,13 @@ runRofi='rofi -dmenu -p -i -config ~/.config/rofi/styles/start-rofi.rasi'
 scriptLocation=~/.config/rofi/scripts
 
 function main() {
-    rofiOption=$(echo -e "\n⏻\n󰵆" | $runRofi)
+    rofiOption=$(echo -e "\n⏻\n󰀻\n" | $runRofi)
 
     case "$rofiOption" in
     "") sh $scriptLocation/wifi-rofi.sh ;;
     "⏻") sh $scriptLocation/power-rofi.sh ;;
-    "󰵆") rofi -show drun -config ~/.config/rofi/styles/application-rofi.rasi ;;
+    "󰀻") rofi -show drun -config ~/.config/rofi/styles/application-rofi.rasi ;;
+    "") rofi -show calc -modi calc -no-show-match -no-sort -config ~/.config/rofi/styles/application-rofi.rasi ;;
     *) exit 1 ;;
     esac
 }

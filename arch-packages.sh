7c31705f3291fc07081995ab05876d64ecd3bef8
Druxorey <druxorey@gmail.com>
2024-12-15 01:28:03 -0400

    feat(arch-packages): updated packages list.

--- arch-packages.sh
+++ arch-packages.sh
@@ -2,12 +2,12 @@
 
 basePacmanList=(bspwm git kitty libinput lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance networkmanager nitrogen picom polybar reflector sxhkd timeshift ufw xorg)
 
-utilsPacmanList=(bash-completion bat btop cmatrix cmus curl exfat-utils eza fastfetch ffmpeg fd flatpak fuse-exfat fzf github-cli gnome-keyring hugo imagemagick ncdu neovim nodejs npm p7zip pandoc-cli php pv ranger redshift ripgrep rofi rsync rust scrot silicon sl speedtest-cli sxiv tar the_silver_searcher thefuck tldr tmux tokei translate-shell trash-cli unrar unzip usbutils vim viu wget xclip xsel yt-dlp zip zoxide zathura zathura-pdf-mupdf)
+utilsPacmanList=(bash-completion bat btop cmatrix cmus curl exfat-utils eza fastfetch ffmpeg fd flatpak fuse-exfat fzf github-cli gnome-keyring hugo imagemagick ncdu neovim nodejs npm p7zip pandoc-cli php pv ranger redshift ripgrep rofi rofi-calc rsync rust scrot silicon sl speedtest-cli sxiv tar the_silver_searcher thefuck tldr tmux tokei translate-shell trash-cli unrar unzip usbutils vim viu wget wine xclip xdg-ninja xsel yt-dlp zip zoxide zathura zathura-pdf-mupdf)
 
-driversPacmanList=(alsa-utils blueman bluez-utils brightnessctl cups cups-pdf ffmpegthumbnailer gestures gvfs-mtp gvfs-smb hplip jdk-openjdk linux-headers mtpfs noto-fonts noto-fonts-cjk noto-fonts-emoji ntfs-3g papirus-icon-theme pulseaudio-bluetooth python-pip python-setuptools python-virtualenv samba simple-scan smbclient system-config-printer tlp tumbler) 
+driversPacmanList=(alsa-utils autorandr blueman bluez-utils brightnessctl cups cups-pdf ffmpegthumbnailer gestures gvfs-mtp gvfs-smb hplip jdk-openjdk linux-headers mtpfs ncurses noto-fonts noto-fonts-cjk noto-fonts-emoji ntfs-3g papirus-icon-theme pulseaudio-bluetooth python-pip python-setuptools python-virtualenv samba simple-scan smbclient system-config-printer tlp tumbler) 
 
-guiPacmanList=(code discord gedit gimp gloobus-preview gthumb libreoffice-fresh nautilus obs-studio obsidian sigil steam thunar vbam-wx virtualbox vlc)
+guiPacmanList=(code discord gedit gimp gloobus-preview gthumb libreoffice-fresh nautilus obs-studio obsidian sigil steam thunar transmission-gtk vbam-wx virtualbox vlc)
 
-yayPackagesList=(ani-cli brave-bin cava googler gtypist manga-cli-git microsoft-edge-dev-bin minecraft-launcher notion-app-electron oh-my-posh peaclock pipes.sh tetris-terminal-git tumbler-extra-thumbnailers)
+yayPackagesList=(ani-cli brave-bin cava googler deemix-gui-git gtypist manga-cli-git minecraft-launcher notion-app-electron oh-my-posh peaclock pipes.sh proton-vpnn-gtk-app tetris-terminal-git tumbler-extra-thumbnailers)
 
 servicesList=(ufw.service cups NetworkManager lightdm bluetooth.service)

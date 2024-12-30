37d35d819fef15cb2b5fb0ee320aefdf5b9b7e1f
Druxorey <druxorey@gmail.com>
2024-12-30 02:20:08 -0400

    feat(nvim): added keymaps to manage git and telescope.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -33,11 +33,27 @@ vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent
 vim.keymap.set("n", "<leader>ai", ":CopilotChat<CR>", { noremap = true, silent = true, desc = "Open Copilot Chat"})
 vim.keymap.set("v", "<leader>ai", ":'<,'>CopilotChat<CR>", { noremap = true, silent = true, desc = "Open Copilot Chat" })
 
--- Git vertical
-vim.keymap.set("n", "<leader>gg", ":vertical Git<CR>", { noremap = true, silent = true, desc = "Git Changes" })
+-- Silicon toggle
+vim.keymap.set("v", "<leader>si", ":'<,'>Silicon<CR>", { noremap = true, silent = true, desc = "Take a screenshot" })
 
--- TAB para moverse al siguiente buffer, solo si no estamos en VSCode
+-- Git
+vim.keymap.set("n", "<leader>gg", ":vertical Git<CR>", { noremap = true, silent = true, desc = "Git Changes" })
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true, desc = "Git Commits" })
+vim.keymap.set("n", "<leader>gs", ":Git status<CR>", { noremap = true, silent = true, desc = "Git Status" })
+vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { noremap = true, silent = true, desc = "Git Log" })
+
+-- TAB to move to the next buffer
 if vim.g.vscode == nil then
 	vim.keymap.set("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
 end
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files , { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fx', function()
+	builtin.find_files({ cwd = '~' })
+end, { desc = 'Telescope find files (Home Dir)' })

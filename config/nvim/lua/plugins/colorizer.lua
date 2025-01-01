2e89c17d5f31311bc6d4a3fd1aeeda7ba9437553
Druxorey <druxorey@gmail.com>
2025-01-01 03:54:17 -0400

    feat(nvim): add lazy loading and configuration for nvim-colorizer.lua

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,3 +1,15 @@
 return {
 	"norcalli/nvim-colorizer.lua",
+	dependencies = {},
+	event = "VeryLazy",
+	opts = {
+		-- The filetypes that should be colorized
+		'*';
+		-- The plugin will not load in these filetypes
+		'!vim';
+		html = { names = false; }
+	},
+	config = function()
+		require("colorizer").setup()
+	end
 }

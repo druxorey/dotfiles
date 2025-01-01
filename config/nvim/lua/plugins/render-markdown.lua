889019f84b4a62b55fb583335608e7e105f09f2d
Druxorey <druxorey@gmail.com>
2025-01-01 03:59:14 -0400

    feat(nvim): add lazy loading for render-markdown.

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,4 +1,12 @@
 return {
 	'MeanderingProgrammer/render-markdown.nvim',
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter',
+		'echasnovski/mini.icons'
+	},
+	lazy = true,
+
+	config = function()
+		require("render-markdown").setup()
+	end
 }

cc89e994eefe60dc44f29e3dfbc3d0b9e52e9aa3
Druxorey <druxorey@gmail.com>
2025-01-01 04:02:27 -0400

    feat(nvim): update nvim-silicon configuration with additional options.

--- config/nvim/lua/plugins/nvim-silicon.lua
+++ config/nvim/lua/plugins/nvim-silicon.lua
@@ -3,9 +3,17 @@ return {
 	lazy = true,
 	cmd = "Silicon",
 	opts = {
-		theme = "Dracula",
 		no_window_controls = true,
 		to_clipboard = true,
+		no_line_number = false,
+
+		theme = "Dracula",
+		background = "#BD93F9",
+
+		shadow_blur_radius = 16,
+		shadow_offset_x = 8,
+		shadow_offset_y = 8,
+		shadow_color = "#282A36",
 
 		output = function()
 			local png_path = "~/Pictures/screenshots/code-screenshots/" .. os.date("!%Y-%m-%d_%H%M%S") .. "_code.png"

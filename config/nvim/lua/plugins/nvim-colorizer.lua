c87f3daaecf516c4fb2604034f7ed056bff1f4af
Druxorey <druxorey@gmail.com>
2025-01-03 17:37:22 -0400

    fix(nvim): removed 'Verylazy' lazyvim event in nvim-colorizer.lua.

--- config/nvim/lua/plugins/nvim-colorizer.lua
+++ config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,12 +1,10 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	dependencies = {},
-	event = "VeryLazy",
 	opts = {
 		-- The filetypes that should be colorized
 		'*';
 		-- The plugin will not load in these filetypes
-		'!vim';
 		html = { names = false; }
 	},
 	config = function()

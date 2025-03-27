3f40fc6decc5f1adae3c96a79772993021907b25
Druxorey <druxorey@gmail.com>
2025-03-26 22:06:24 -0400

    feat(nvim): add preview toggle and update keymaps

--- config/nvim/lua/plugins/lazy-neotree.lua
+++ config/nvim/lua/plugins/lazy-neotree.lua
@@ -6,15 +6,24 @@ return {
 		"MunifTanjim/nui.nvim"
 	},
 	keys = {
-		{"<leader>E", false},
+		{ "<leader>E", false },
+		{ "<leader>e", false },
 	},
 	opts = {
 		window = {
-			position = "left",
+			position = "right",
 			width = 30,
 			mapping_options = {
 				noremap = true,
 				nowait = true
+			},
+			mappings = {
+				["P"] = {
+					"toggle_preview",
+					config = {
+						use_float = false,
+					},
+				},
 			}
 		}
 	}

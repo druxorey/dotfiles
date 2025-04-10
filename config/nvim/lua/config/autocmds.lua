83126b863b7024fada8b16a4c85da6903c3b8d31
Druxorey <druxorey@gmail.com>
2025-04-10 17:44:57 -0400

    feat(nvim): add autocmd for CSV file type support

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "csv",
+    callback = function()
+        vim.cmd("CsvViewEnable")
+    end,
+})

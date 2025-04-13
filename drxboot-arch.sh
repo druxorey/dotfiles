1b66d152bdd51159c2a0824d578ca4eeca20bedc
Druxorey <druxorey@gmail.com>
2025-04-12 21:42:45 -0400

    feat(arch-bootstrap): integrate dialog for TUI-based interactions
    - Replaced manual prompts with dialog-based TUI for better UX.
    - Added `waitScreen` function to display progress during tasks.
    - Introduced `dialog --gauge` for dependency installation progress.
    - Added dialog-based confirmation for system updates and AUR installs.
    - Implemented menu selection for installation types (minimal/desktop).
    - Added dialog-based prompts for zsh and dotfiles installation.
    - Ensured dialog is installed before script execution.
    - Modularized functions for better readability and maintainability.

--- drxboot-arch.sh
+++ drxboot-arch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 TITLE="\e[1;35m"
 RUNNING="\e[35m"
 QUESTION="\e[36m"
@@ -8,7 +11,7 @@ FAILED="\e[1;31m"
 END="\e[0m"
 LINE="\n"
 
-isAutomated=0
+IS_AUTOMATED=0
 
 function help() {
 	echo
@@ -29,201 +32,242 @@ function help() {
 	exit 1
 }
 
-
-function initialize() {
-	if ! command -v wget &> /dev/null; then
-		sudo pacman -S --noconfirm wget
-	fi
-
-	if ! command -v rsync &> /dev/null; then
-		sudo pacman -S --noconfirm rsync
-	fi
-
-	if ! command -v unzip &> /dev/null; then
-		sudo pacman -S --noconfirm unzip
-	fi
-
-	test -f arch-packages.sh || wget -O arch-packages.sh https://raw.githubusercontent.com/druxorey/dotfiles/master/arch-packages.sh
-	source "$(dirname "$0")/arch-packages.sh"
-	clear
+function waitScreen() {
+	local title="$1"
+	dialog --title "$title" --infobox "\n           Installing, please wait..." 5 50
 }
 
 
-function installPackage() {
-	local pacman="sudo pacman -S --noconfirm"
+function installDependencies() {
+
+	if [[ -f /var/lib/pacman/db.lck ]]; then
+		sudo rm /var/lib/pacman/db.lck
+	fi
+
+	dependencies=(
+		"git"
+		"wget"
+		"rsync"
+		"unzip"
+	)
+
+	local totalDependencies=$(( ${#dependencies[@]} + 1))
+	local titleStart="  WELCOME TO DRUXOREY'S ARCH LINUX BOOTSTRAP  "
+	local descriptionStart="\n               Starting Script..."
+	dialog --title "$titleStart" --gauge "$descriptionStart" 8 50 0 < <(
+		for index in "${!dependencies[@]}"; do
+			package="${dependencies[$index]}"
+
+			if ! command -v $package &> /dev/null; then
+				sudo pacman -S --noconfirm --needed $package > /dev/null 2>&1
+			fi
+
+			echo $(( (index + 1) * 100 / totalDependencies))
+			sleep 0.125
+		done
+	)
+
+	arch_deps_content=$(curl -s https://raw.githubusercontent.com/druxorey/dotfiles/master/arch-deps.pkglist)
+	eval "$arch_deps_content"
+}
+
+
+function installPackages() {
+	local type="$1"
+	local pacman="sudo pacman -S --noconfirm --needed"
 	local yay="yay -S --noconfirm"
-	local packageManager="$1"
-	shift
 
-	if [ "$packageManager" == "pacman" ]; then
-		commandExecutor="$pacman"
-	elif [ "$packageManager" == "yay" ]; then
-		commandExecutor="$yay"
-	fi
+	local -n main_packages="${type}_main_packages"
+	local totalPackages=${#main_packages[@]}
+	local descInstallation="\n                 Please wait..."
 
-	for package in "${@}"; do
-		echo -e "${RUNNING}Installing ${END}$package"
-		$commandExecutor $package
-		if [ $? -eq 0 ]; then
-			echo -e "${SUCCESS}The package has been installed correctly${END}"
-		else
-			echo -e "${FAILED} !WARNING! The following package could not be installed: ${END}$package"
-		fi
-	done
+	local titleInstallation="INSTALLING PACMAN PACKAGES"
+	dialog --title "$titleInstallation" --gauge "$descInstallation" 8 50 0 < <(
+		for index in "${!main_packages[@]}"; do
+			package="${main_packages[$index]}"
+			echo $(( (index + 1) * 100 / totalPackages))
+			$pacman $package > /dev/null 2>&1
+			sleep 0.125
+		done
+	)
+
+	local -n aur_packages="${type}_aur_packages"
+	local totalPackages=${#main_packages[@]}
+
+	local titleInstallation="INSTALLING YAY PACKAGES"
+	dialog --title "$titleInstallation" --gauge "$descInstallation" 8 50 0 < <(
+		for index in "${!main_packages[@]}"; do
+			package="${main_packages[$index]}"
+			echo $(( (index + 1) * 100 / totalPackages))
+			$pacman $package > /dev/null 2>&1
+			sleep 0.125
+		done
+	)
 }
 
 
-function enableService() {
+function enableServices() {
 	local enableCommand="sudo systemctl enable"
-	shift
+	local totalServices=${#drivers_Services[@]}
 
-	for service in "${@}"; do
-		echo -e "${RUNNING}Enabling ${END}$service"
-		$enableCommand $service
-		if [ $? -eq 0 ]; then
-			echo -e "${SUCCESS}The service has been enabled correctly${END}"
-		else
-			echo -e "${FAILED} !WARNING! The following service could not be enabled: ${END}$service"
-		fi
-	done
+	local titleServices="ENABLING SERVICES"
+	local desc="Please wait..."
+
+	dialog --title "$titleServices" --gauge "$descInstallation" 8 50 0 < <(
+		for index in "${!drivers_services[@]}"; do
+			service="${drivers_Services[$index]}"
+
+			if [[ $(sudo systemctl is-active "$service") == "inactive" ]]; then
+				$enableCommand $service > /dev/null 2>&1
+			fi
+
+			echo $(( (index + 1) * 100 / totalServices))
+			sleep 0.125
+		done
+	)
 }
 
 
 function copyDotfiles() {
-	local dotfilesPath="6PYqO7W9oEQJb04nSoqajW26Xexnbw3z/"
-	local gitClone="git clone https://github.com/druxorey/dotfiles.git $dotfilesPath"
+	(
+		local dotfilesPath="6PYqO7W9oEQJb04nSoqajW26Xexnbw3z/"
+		local gitClone="git clone https://github.com/druxorey/dotfiles.git $dotfilesPath"
 
-	[ ! -d "$HOME/.config" ] && mkdir "$HOME/.config"
-	[ ! -d "$HOME/.local" ] && mkdir "$HOME/.local"
-	[ ! -d "$HOME/.local/share" ] && mkdir "$HOME/.local/share"
-	[ ! -d "$HOME/.local/share/icons" ] && mkdir "$HOME/.local/share/icons"
-	[ ! -d "$HOME/.local/share/themes" ] && mkdir "$HOME/.local/share/themes"
+		[ ! -d "$HOME/.config" ] && mkdir "$HOME/.config"
+		[ ! -d "$HOME/.local" ] && mkdir "$HOME/.local"
+		[ ! -d "$HOME/.local/share" ] && mkdir "$HOME/.local/share"
+		[ ! -d "$HOME/.local/share/icons" ] && mkdir "$HOME/.local/share/icons"
+		[ ! -d "$HOME/.local/share/themes" ] && mkdir "$HOME/.local/share/themes"
 
-	if $gitClone; then
-		rsync -av --delete $dotfilesPath/config/* $HOME/.config/
-		mv -f $HOME/.config/zshrc $HOME/.zshrc
-		mv -f $HOME/.config/bashrc $HOME/.bashrc
-		rm -rf $dotfilesPath/
-		echo -e "${SUCCESS}The dotfiles have been cloned correctly${END}"
-	else
-		echo -e "${FAILED} !WARNING! The dotfiles could not be cloned"
-	fi
+		if $gitClone; then
+			rsync -av --delete $dotfilesPath/config/* $HOME/.config/
+			mv -f $HOME/.config/zshrc $HOME/.zshrc
+			mv -f $HOME/.config/bashrc $HOME/.bashrc
+			rm -rf $dotfilesPath/
+			echo -e "${SUCCESS}The dotfiles have been cloned correctly${END}"
+		else
+			echo -e "${FAILED} !WARNING! The dotfiles could not be cloned"
+		fi
 
-	wget "https://github.com/dracula/gtk/archive/master.zip" -O master.zip
-	wget "https://github.com/dracula/gtk/files/5214870/Dracula.zip" -O Dracula.zip
+		wget "https://github.com/dracula/gtk/archive/master.zip" -O master.zip
+		wget "https://github.com/dracula/gtk/files/5214870/Dracula.zip" -O Dracula.zip
 
-	if [[ -f "master.zip" && -f "Dracula.zip" ]]; then
-		unzip Dracula.zip
-		mv -f Dracula $HOME/.local/share/icons/
-		rm -rf Dracula.zip
-		unzip master.zip
-		mv gtk-master Dracula
-		mv -f Dracula $HOME/.local/share/themes/
-		rm -rf master.zip
-		echo -e "${SUCCESS}The gtk theme has been cloned correctly${END}"
-	else
-		echo -e "${FAILED} !WARNING! The gtk theme could not be cloned"
-	fi
+		if [[ -f "master.zip" && -f "Dracula.zip" ]]; then
+			unzip Dracula.zip
+			mv -f Dracula $HOME/.local/share/icons/
+			rm -rf Dracula.zip
+			unzip master.zip
+			mv gtk-master Dracula
+			mv -f Dracula $HOME/.local/share/themes/
+			rm -rf master.zip
+			echo -e "${SUCCESS}The gtk theme has been cloned correctly${END}"
+		else
+			echo -e "${FAILED} !WARNING! The gtk theme could not be cloned"
+		fi
 
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting
-	git clone https://github.com/zsh-users/zsh-autosuggestions
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting
+		git clone https://github.com/zsh-users/zsh-autosuggestions
 
-	if [[ -d "zsh-syntax-highlighting" && -d "zsh-autosuggestions" ]]; then
-		mv -f zsh-syntax-highlighting $HOME/.config/zsh/zsh-syntax-highlighting
-		mv -f zsh-autosuggestions $HOME/.config/zsh/zsh-autosuggestions
-		echo -e "${SUCCESS}The zsh plugins have been cloned correctly${END}"
-	else
-		echo -e "${FAILED} !WARNING! The zsh plugins could not be cloned"
-	fi
+		if [[ -d "zsh-syntax-highlighting" && -d "zsh-autosuggestions" ]]; then
+			mv -f zsh-syntax-highlighting $HOME/.config/zsh/zsh-syntax-highlighting
+			mv -f zsh-autosuggestions $HOME/.config/zsh/zsh-autosuggestions
+			echo -e "${SUCCESS}The zsh plugins have been cloned correctly${END}"
+		else
+			echo -e "${FAILED} !WARNING! The zsh plugins could not be cloned"
+		fi
 
-	git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
+		git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 
-	if [[ -d "$HOME/.config/tmux/plugins/tpm" ]]; then
-		echo -e "${SUCCESS}The tmux plugin has been cloned correctly${END}"
-	else
-		echo -e "${FAILED} !WARNING! The tmux plugin could not be cloned"
-	fi
-}
-
-
-function askUser() {
-	local question="$1"
-	local action="$2"
-
-	if [[ $isAutomated -eq 0 ]]; then
-		echo -ne "${QUESTION} $question [Y/n]: ${END}"
-		read -r output
-		echo
-		output=${output,,}
-	fi
-
-	[[ -z $output ]] && output='y'
-
-	if [[ $output == 'y' ]]; then
-		eval "$action"
-	else
-		echo -e "${FAILED} ⚠ !The installation will not be executed!${END}"
-	fi
+		if [[ -d "$HOME/.config/tmux/plugins/tpm" ]]; then
+			echo -e "${SUCCESS}The tmux plugin has been cloned correctly${END}"
+		else
+			echo -e "${FAILED} !WARNING! The tmux plugin could not be cloned"
+		fi
+	)
 }
 
 
 function main() {
-
 	while getopts ":ah" opt; do
 		case ${opt} in
-			a ) isAutomated = 1 ;;
+			a ) IS_AUTOMATED=1 ;;
 			h ) help ;;
 			* ) echo "Invalid Option: -$OPTARG" 1>&2 ; exit 1 ;;
 		esac
 	done
 
-	initialize
-
 	shift $((OPTIND -1))
 
-	echo -e "${LINE}${TITLE} [========== WELCOME TO DRUXOREY'S ARCH LINUX BOOTSTRAP ==========] ${END}${LINE}"
+	if ! command -v dialog &> /dev/null; then
+		echo -e "Waiting for dialog to be installed..."
+		sudo pacman -S --noconfirm --needed dialog > /dev/null 2>&1
+	fi
 
-	installCommand='sudo pacman -Syu'
-	echo -e "It is necessary to update the system to ensure the correct functioning of the script."
-	askUser "► Proceed with the update?" "$installCommand"
+	installDependencies
 
-	installCommand="cd && git clone https://aur.archlinux.org/yay.git && cd yay/ && makepkg -si && cd .. && sudo rm -r yay/"
-	askUser "► Do you want to install yay?" "$installCommand"
+	local descUpdateSystem="\nIt is necessary to update the system to ensure the correct functioning of the script. \n\n            Do you want to update?"
+	dialog --clear --title "" --yesno "$descUpdateSystem" 10 50
+	local responseUpdateSystem=$?
 
-	clear
-	echo -e "${TITLE} [========== PACKAGE INSTALLATION ==========]"
+	if ! command -v yay &> /dev/null; then
+		local titleAurInstallation="AUR INSTALLATION REQUIRED"
+		local descAurInstallation="\n         Do you want to install yay?"
+		dialog --clear --title "" --yesno "$descAurInstallation" 7 50
+		responseAurInstallation=$?
+	fi
 
-	pacmanCategories=("core" "essential" "utils" "drivers" "customization" "gui")
-	for category in "${pacmanCategories[@]}"; do
-		installCommand="installPackage "pacman" "\${${category}_Packages[@]}""
-		echo -e "${LINE}${RUNNING}The next pacman packages will be installing:${END} $(eval echo \${${category}_Packages[@]})"
-		askUser "► Proceed with the installation?" "$installCommand"
-	done
+	local descInstallationType="\nSelect the installation type you want to perform:\n "
+	local responseTypeInstallation=$(dialog --clear --no-cancel --title "" --menu "$descInstallationType" 13 50 2 \
+		1 "Minimal" \
+		2 "Desktop" 2>&1 >/dev/tty)
 
-	aurCategories=("terminal" "gui")
-	for category in "${aurCategories[@]}"; do
-		installCommand="installPackage "yay" "\${${category}_AurPackages[@]}""
-		echo -e "${LINE}${RUNNING}The next pacman packages will be installing:${END} $(eval echo \${${category}_AurPackages[@]})"
-		askUser "► Proceed with the installation?" "$installCommand"
-	done
+	local descZshInstallation="\nDo you want to install zsh and make it the default shell?"
+	dialog --clear --title "" --yesno "$descZshInstallation" 8 50
+	local responseZshInstallation=$?
 
-	clear
-	echo -e "${TITLE} [========== SERVICE ENABLEMENT ==========]"
+	local descDotfilesInstallation="\n    Do you want to install the dotfiles?"
+	dialog --clear --title "" --yesno "$descDotfilesInstallation" 7 50
+	local responseDotfilesInstallation=$?
 
-	installCommand='enableService "${drivers_Services[@]}"'
-	askUser "► Proceed enabling the services?" "$installCommand"
+	if [[ $responseUpdateSystem == 0 ]]; then
+		waitScreen "UPDATING SYSTEM"
+		sudo pacman -Syu --noconfirm > /dev/null 2>&1
+	fi
 
-	clear
-	echo -e "${TITLE} [========== ZSH INSTALLATION ==========]"
+	if [[ $responseAurInstallation == 0 ]]; then
+		(
+			waitScreen "INSTALLING YAY"
+			sleep 3
+			clear
+			cd /tmp
+			git clone https://aur.archlinux.org/yay.git
+			cd yay
+			makepkg -si --noconfirm
+		)
+	fi
 
-	installCommand='sudo pacman -S --noconfirm zsh && chsh -s /bin/zsh'
-	askUser "► Proceed installing zsh and make it the default shell?" "$installCommand"
+	case $responseTypeInstallation in
+		1)
+			installPackages "minimal"
+			;;
+		2)
+			installPackages "minimal"
+			installPackages "desktop"
+			;;
+	esac
 
-	clear
-	echo -e "${TITLE} [========== CUSTOMIZATION ==========]"
+	enableServices
 
-	echo -e "${FAILED} ⚠ !WARNING THE NEXT COMMAND WILL OVERRIDE EXISTING FILES!${END}"
-	askUser "► Proceed copying the dotfiles?" "copyDotfiles"
+	if [[ $responseZshInstallation == 0 ]]; then
+		waitScreen "INSTALLING ZSH"
+		sudo pacman -S --noconfirm zsh > /dev/null 2>&1
+		sudo chsh -s /bin/zsh
+	fi
+
+	if [[ $responseDotfilesInstallation == 0 ]]; then
+		waitScreen "INSTALLING DOTFILES"
+		copyDotfiles > /dev/null 2>&1
+	fi
 
 	clear
 

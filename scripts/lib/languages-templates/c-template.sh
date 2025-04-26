b2c09727696dde9caa54e67f5731f4e81dac1fb4
Druxorey <druxorey@gmail.com>
2025-04-26 16:08:46 -0400

    fix(scripts): remove extra newline in printf

--- scripts/lib/languages-templates/c-template.sh
+++ scripts/lib/languages-templates/c-template.sh
@@ -14,7 +14,7 @@ codeTemplate=$(cat << EOF
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	printf("\n\n\e[0;35m[========= EXERCISE =========]\e[0m\n\n");
+	printf("\n\e[0;35m[========= EXERCISE =========]\e[0m\n\n");
 	return 0;
 }
 EOF

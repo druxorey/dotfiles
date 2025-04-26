045aad45a0513272cbb0da2219875db7db54cc2f
Druxorey <druxorey@gmail.com>
2025-04-26 16:08:02 -0400

    feat(rofi): enhanced the Wi-Fi connection script to provide better user feedback

--- config/rofi/scripts/wifi-rofi.sh
+++ config/rofi/scripts/wifi-rofi.sh
@@ -5,57 +5,62 @@ rofiNewSSID='rofi -dmenu -p -i -config ~/.config/rofi/styles/wifi/wifi-newssid.r
 rofiNewPassword='rofi -dmenu -p -i -config ~/.config/rofi/styles/wifi/wifi-newpassword.rasi'
 
 function connectToWifi() {
-    local nameWifi="$1"
-    local passwordWifi="$2"
-    local command="nmcli dev wifi con $nameWifi hidden yes"
+	local nameWifi="$1"
+	local passwordWifi="$2"
+	local command="nmcli dev wifi con $nameWifi hidden yes"
 
-    [[ -n "$passwordWifi" ]] && command="nmcli dev wifi con $nameWifi password $passwordWifi hidden yes"
+	[[ -n "$passwordWifi" ]] && command="nmcli dev wifi con $nameWifi password $passwordWifi hidden yes"
 
-    for ((i = 1; i <= 5; i++)); do
-        if $command; then
-			notify-send -u low "Successfully connected to the WiFi network '$nameWifi'."
-			exit
-        fi
-        echo "The command failed, retrying..."
-        sleep 3
-    done
+	notify_id=8000
+	percentage=0
+	notify-send -u low -r $notify_id "Connecting to $nameWifi..."
 
-	notify-send -u critical "Failed to establish connection. Please check the WiFi name and password."
+	for ((i = 1; i <= 5; i++)); do
+		percentage=$((i * 20))
+		notify-send -u low -r $notify_id -h int:value:${percentage} "Connecting to $nameWifi..."
+		if $command; then
+			notify-send -u low -r $notify_id "Successfully connected to $nameWifi."
+			return
+		fi
+		echo "The command failed, retrying..."
+		sleep 3
+	done
+
+	notify-send -u critical "Failed to establish connection"
 }
 
 function main() {
-    wifiStatus=$(nmcli -fields WIFI g)
-    savedConnections=$(nmcli -t -f NAME connection show | awk 'NR!=2 {print "   " $0}')
+	wifiStatus=$(nmcli -fields WIFI g)
+	savedConnections=$(nmcli -t -f NAME connection show | awk 'NR!=2 {print "   " $0}')
 
-    if [[ "$wifiStatus" =~ "enabled" ]]; then
-        isToggled="󰤮   Disable Wi-Fi"
-    elif [[ "$wifiStatus" =~ "disabled" ]]; then
-        isToggled="󰤨   Enable Wi-Fi"
-    fi
+	if [[ "$wifiStatus" =~ "enabled" ]]; then
+		isToggled="󰤮   Disable Wi-Fi"
+	elif [[ "$wifiStatus" =~ "disabled" ]]; then
+		isToggled="󰤨   Enable Wi-Fi"
+	fi
 
-    rofiOption=$(echo -e "󱛃   New Connection\n$isToggled\n$savedConnections" | $rofiMenu)
+	rofiOption=$(echo -e "󱛃   New Connection\n$isToggled\n$savedConnections" | $rofiMenu)
 
-    if [ "$rofiOption" = "󰤨   Enable Wi-Fi" ]; then
-        nmcli radio wifi on
-        exit
-    elif [ "$rofiOption" = "󰤮   Disable Wi-Fi" ]; then
-        nmcli radio wifi off
-        exit
-    fi
+	if [ "$rofiOption" = "󰤨   Enable Wi-Fi" ]; then
+		nmcli radio wifi on
+		exit
+	elif [ "$rofiOption" = "󰤮   Disable Wi-Fi" ]; then
+		nmcli radio wifi off
+		exit
+	fi
 
-    if [ "$rofiOption" = "󱛃   New Connection" ]; then
-        nameWifi=$($rofiNewSSID)
-        passwordWifi=$($rofiNewPassword)
+	nmcli dev wifi rescan
 
-		if [ -z "$nameWifi" ] || [ -z "$passwordWifi" ]; then
-			exit
-		fi
+	if [ "$rofiOption" = "󱛃   New Connection" ]; then
+		nameWifi=$($rofiNewSSID)
+		passwordWifi=$($rofiNewPassword)
+		[ -z "$nameWifi" ] || [ -z "$passwordWifi" ] && exit
 	else
 		nameWifi=$(echo "$rofiOption" | sed 's/^   //' | awk -F ":" '{print $1}')
 		passwordWifi=$(echo "$rofiOption" | sed 's/^   //' | awk -F ":" '{print $2}')
-    fi
+	fi
 
-    [[ -n $rofiOption ]] && connectToWifi $nameWifi $passwordWifi
+	[[ -n $rofiOption ]] && connectToWifi $nameWifi $passwordWifi
 }
 
 main $@

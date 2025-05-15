29aedfbd357a44b1c466d8e72a0ab2689520d521
Druxorey <druxorey@gmail.com>
2025-05-15 02:28:47 -0400

    feat(rofi): integrate recursive file browser

--- config/rofi/scripts/energy_menu.sh
+++ config/rofi/scripts/energy_menu.sh
@@ -21,12 +21,15 @@ function main() {
 	case "$rofiOption" in
 		"󱟧")
 			configFile="$CONFIG_DIR/tlp.d/1-powersave.conf"
+			powerPlan="powersave"
 			;;
 		"󱐋")
 			configFile="$CONFIG_DIR/tlp.d/2-performance.conf"
+			powerPlan="performance"
 			;;
 		"󰥔")
 			configFile="$CONFIG_DIR/tlp.d/3-normal.conf"
+			powerPlan="normal"
 			;;
 		*)
 			echo -e "\e[1;31mERROR: Invalid option"
@@ -37,6 +40,7 @@ function main() {
 	if [[ -f "$configFile" ]]; then
 		sudo ln -sf "$configFile" "$CONFIG_DIR/tlp.conf" && echo "Configuration changed to: $configFile"
 		sudo tlp start && echo "TLP restarted with the new configuration"
+		notify-send -u low "Changed energy plan to $powerPlan"
 	else
 		echo -e "\e[1;31mERROR: The configuration file '$configFile' does not exist"
 		exit 1
--- config/rofi/scripts/start_menu.sh
+++ config/rofi/scripts/start_menu.sh
@@ -4,11 +4,12 @@ SCRIPTS_PATH=~/.config/rofi/scripts
 
 function main() {
 	runRofi='rofi -dmenu -p -i -config ~/.config/rofi/styles/start_styles.rasi'
-	rofiOption=$(echo -e "\n󰀻\n󰂄\n⏻" | $runRofi)
+	rofiOption=$(echo -e "\n󰀻\n󰱼\n󰂄\n⏻" | $runRofi)
 
 	case "$rofiOption" in
 		"") sh $SCRIPTS_PATH/wifi_menu.sh ;;
 		"󰀻") rofi -show drun -config ~/.config/rofi/styles/launcher_styles.rasi;;
+		"󰱼") rofi -show recursivebrowser -config ~/.config/rofi/styles/files_styles.rasi;;
 		"󰂄") sh $SCRIPTS_PATH/energy_menu.sh ;;
 		"⏻") sh $SCRIPTS_PATH/power_menu.sh ;;
 		*) exit 1 ;;

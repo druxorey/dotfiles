08802d4d33543c577333b9fc744793047ddab128
Druxorey <druxorey@gmail.com>
2025-09-11 19:31:27 -0400

    feat(polybar): redesign bar with enhanced borders and layout:
    - Added new border modules (R1, L1, R2, L2) for improved visuals
    - Updated section theming with consistent foreground/background
    - Refined padding and spacing for better alignment and balance
    - Improved module labels and icons for clearer representation
    - Adjusted network and battery modules for better readability

--- config/polybar/scripts/get-cmus.sh
+++ config/polybar/scripts/get-cmus.sh
@@ -4,22 +4,22 @@ ARTIST_MAX_LENGTH=50
 
 status=$(cmus-remote -Q 2>/dev/null)
 if [ $? -ne 0 ]; then
-    echo ""
-    exit 0
+	printf ""
+	exit 0
 fi
 
-state=$(echo "$status" | grep "status" | awk '{print $2}')
-artist=$(echo "$status" | grep "tag artist" | cut -d ' ' -f 3-)
-title=$(echo "$status" | grep "tag title" | cut -d ' ' -f 3-)
+state=$(printf "$status" | grep "status" | awk '{print $2}')
+artist=$(printf "$status" | grep "tag artist" | cut -d ' ' -f 3-)
+title=$(printf "$status" | grep "tag title" | cut -d ' ' -f 3-)
 
 if [ ${#artist} -gt $ARTIST_MAX_LENGTH ]; then
-	artist=$(echo "$artist" | cut -c 1-$ARTIST_MAX_LENGTH)...
+	artist=$(printf "$artist" | cut -c 1-$ARTIST_MAX_LENGTH)...
 fi
 
 if [ "$state" = "playing" ]; then
-    echo " $artist - $title"
+	printf "   $artist - $title"
 elif [ "$state" = "paused" ]; then
-    echo " $artist - $title"
+	printf "   $artist - $title"
 else
-    echo " Stopped"
+	printf "   Stopped"
 fi

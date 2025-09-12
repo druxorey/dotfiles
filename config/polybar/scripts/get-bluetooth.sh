9761bad5b77a3dde0372bb098975302a24ef9b3c
Druxorey <druxorey@gmail.com>
2025-09-11 22:01:08 -0400

    feat(polybar): add bluetooth module with status script.

---
+++ config/polybar/scripts/get-bluetooth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function main() {
+	status=$(bluetoothctl show 2>/dev/null)
+
+	if [ $? -ne 0 ]; then
+		printf "󰂲"
+	elif echo "$status" | grep -q "Endpoint"; then
+		printf "󰂱"
+	else
+		printf "󰂯"
+	fi
+}
+
+main $@

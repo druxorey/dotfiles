7acb8a0b68a5bd23ee0941e73e9ac48c112fed6e
Druxorey <druxorey@gmail.com>
2025-10-02 21:45:44 -0400

    fix(rofi): correct icon for notes menu option

--- config/rofi/scripts/start_menu.sh
+++ config/rofi/scripts/start_menu.sh
@@ -4,11 +4,11 @@ SCRIPTS_PATH=~/.config/rofi/scripts
 
 function main() {
 	runRofi='rofi -dmenu -p -i -config ~/.config/rofi/styles/start_styles.rasi'
-	rofiOption=$(echo -e "\n󰀻\n󰱼\n󰂄\n⏻" | $runRofi)
+	rofiOption=$(echo -e "\n󰈙\n󰱼\n󰂄\n⏻" | $runRofi)
 
 	case "$rofiOption" in
 		"") sh $SCRIPTS_PATH/wifi_menu.sh ;;
-		"󰀻") sh $SCRIPTS_PATH/notes_menu.sh ;;
+		"󰈙") sh $SCRIPTS_PATH/notes_menu.sh ;;
 		"󰱼") rofi -show recursivebrowser -config ~/.config/rofi/styles/files_styles.rasi ;;
 		"󰂄") sh $SCRIPTS_PATH/energy_menu.sh ;;
 		"⏻") sh $SCRIPTS_PATH/power_menu.sh ;;

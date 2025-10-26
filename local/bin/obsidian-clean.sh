2e471df59844567e4ec6e7339b5f1c0b09acb9b4
Druxorey <druxorey@gmail.com>
2025-10-26 01:27:28 -0400

    refactor(scripts): unify formatting for script outputs:
    - Standardized output formatting across multiple scripts
    - Replaced inconsistent variables with FORMAT_* constants
    - Improved readability and maintainability of help messages

---
+++ local/bin/obsidian-clean.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VAULT_PATH="${HOME}/Documents/01 Obsidian/"
+
+FORMAT_ERROR="\e[1;31m[ERROR]"
+FORMAT_SUCCESS="\e[1;32m[SUCCESS]"
+FORMAT_END="\e[0m"
+
+function main() {
+	if [ ! -d "${VAULT_PATH}/.git" ]; then
+		printf "$FORMAT_ERROR Obsidian vault not found.$FORMAT_END\n"
+		exit 1
+	fi
+
+	(
+		cd "${VAULT_PATH}" || { printf "$FORMAT_ERROR Failed to change directory.$FORMAT_END"; return; }
+		git checkout --orphan temp_branch || { printf "$FORMAT_ERROR Failed to create orphan branch.$FORMAT_END"; return; }
+		git add -A && git commit -m "Clean branch - $(date +%d-%m-%Y_%H-%M-%S)" || { printf "$FORMAT_ERROR Failed to commit changes.$FORMAT_END"; return; }
+		git branch -D main || { printf "$FORMAT_ERROR Failed to delete main branch.$FORMAT_END"; return; }
+		git branch -m main || { printf "$FORMAT_ERROR Failed to rename branch to main.$FORMAT_END"; return; }
+		git push -f origin main || { printf "$FORMAT_ERROR Failed to push to origin.$FORMAT_END"; return; }
+	)
+
+	printf "$FORMAT_SUCCESS Obsidian vault cleaned.$FORMAT_END\n"
+}
+
+main "$@"

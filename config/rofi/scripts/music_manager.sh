e3dc213e4058109ff40c64e06d50079064fa3596
Druxorey <druxorey@gmail.com>
2026-03-31 04:23:38 -0400

    feat(rofi): add volume control and logic for Lofi:
    - Integrated volume adjustment for Lofi using pactl
    - Added specific Rofi menu options for Lofi radio
    - Improved process management (pgrep/pkill) for Lofi
    - Refactored main loop to handle Lofi and cmus states

--- config/rofi/scripts/music_manager.sh
+++ config/rofi/scripts/music_manager.sh
@@ -10,15 +10,22 @@ declare OPTION=2
 
 function checkLofi() {
 	if pgrep -x "lofi" > /dev/null; then
-		pkill -x "lofi"
-		return 0
+		pkill -x "lofi" || printf "%b Failed to stop Lofi Radio\n" "$FORMAT_ERROR" >&2
 	else
 		cmus-remote --stop
-		kitty lofi
+		kitty lofi 50 &
+		sleep 1
 	fi
 
 	return 0
 }
+
+
+function changeLofiVolume() {
+	printf "%b Changing Lofi volume: %s\n" "$FORMAT_SUCCESS" "$1"
+	pactl set-sink-input-volume "$(pactl list sink-inputs | awk '/Sink Input #/{id=$3} /application.process.binary = "ffplay"/{print id; exit}' | tr -d '#')" $1
+}
+
 
 function getThumbnail() {
 	rm -f "$THUMBNAIL_PATH"
@@ -54,14 +61,16 @@ function main() {
 	local filePath=$(echo "$status" | grep "file" | cut -d ' ' -f 2-)
 	local reproducerState=""
 	local lofiSelected=""
+	local options=""
 
 	printf "State:   %s\nArtist:  %s\nTitle:   %s\nPath:    %s\n" "${state:-N/A}" "${artist:-Unknown}" "${title:-Unknown}" "${filePath:-N/A}"
 
 	local state=$(echo "$status" | grep "status" | awk '{print $2}')
 	if pgrep -x "lofi" > /dev/null; then
 		reproducerState="󰎊"
-		lofiSelected="-u 4"
+		lofiSelected="-u 2"
 		message="Lofi Radio"
+		options="\n󰝞\n󰋋\n󰝝\n"
 	elif [ "$state" = "playing" ]; then
 		reproducerState=""
 	elif [ "$state" = "paused" ]; then
@@ -73,19 +82,31 @@ function main() {
 		message="No music playing"
 	fi
 
+	[[ -z "$options" ]] && options="\n󰒮\n$reproducerState\n󰒭\n󰋋"
 	[[ -z "$lofiSelected" ]] && getThumbnail "$filePath"
 
-	local rofiOption=$(echo -e "\n󰒮\n$reproducerState\n󰒭\n󰋋" | rofi -dmenu -p -i -m -1 $lofiSelected -mesg "$message" -selected-row $OPTION -config $CONFIG_FILE)
+	local rofiOption=$(echo -e "$options" | rofi -dmenu -p -i -m -1 $lofiSelected -mesg "$message" -selected-row $OPTION -config $CONFIG_FILE)
 
-	case "$rofiOption" in
-		"") cmus-remote --stop && OPTION=0 ;;
-		"󰒮") cmus-remote --prev && OPTION=1 ;;
-		"󰒭") cmus-remote --next && OPTION=3 ;;
-		"󰋋") bspc desktop -f ^5 ; checkLofi  & exit 0 ;;
-		"") bspc desktop -f ^5 ; kitty cmus & exit 0 ;;
-		"$reproducerState") cmus-remote --pause && OPTION=2 ;;
-		*) exit 1 ;;
-	esac
+	if pgrep -x "lofi" > /dev/null; then
+		printf "Lofi Radio options selected: %s\n" "$rofiOption"
+		case "$rofiOption" in
+			"󰝞") changeLofiVolume "-10%" && OPTION=1 ;;
+			"󰝝") changeLofiVolume "+10%" && OPTION=3 ;;
+			"󰋋") bspc desktop -f ^5 ; checkLofi  ; OPTION=4 ;;
+			*) printf "%b No option selected. Exiting.\n" "${FORMAT_WARNING}" && exit 0 ;;
+		esac
+	else
+		printf "Music options selected: %s\n" "$rofiOption"
+		case "$rofiOption" in
+			"") cmus-remote --stop && OPTION=0 ;;
+			"󰒮") cmus-remote --prev && OPTION=1 ;;
+			"󰒭") cmus-remote --next && OPTION=3 ;;
+			"󰋋") bspc desktop -f ^5 ; checkLofi  ; OPTION=2 ;;
+			"") bspc desktop -f ^5 ; kitty cmus & OPTION=2 ;;
+			"$reproducerState") cmus-remote --pause && OPTION=2 ;;
+			*) printf "%b No option selected. Exiting.\n" "${FORMAT_WARNING}" && exit 0 ;;
+		esac
+	fi
 
 	return 0
 }

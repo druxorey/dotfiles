bd2b4eea1d720c3104b16c175b0ac531d35bd1f2
Druxorey <druxorey@gmail.com>
2026-05-26 09:45:32 -0400

    feat(polybar): add systray and japanese font support:
    - Integrated internal tray module and removed custom process scripts.
    - Added Noto Sans CJK JP font for extended character support.
    - Updated module layout to include the system tray in the right bar.
    - Deleted obsolete get-processes and kill-processes scripts.

--- config/polybar/scripts/get-cmus.sh
+++ config/polybar/scripts/get-cmus.sh
@@ -3,33 +3,37 @@
 declare -i MAX_LENGTH_TITLE=50
 declare -i MAX_LENGTH_ARTIST=25
 
+function getStatus() {
+    if pgrep -x lofi > /dev/null; then
+        printf "󰋋   Lofi Radio\n"
+        return
+    fi
+
+    local status=$(cmus-remote -Q 2>/dev/null)
+    if [[ -z $status ]]; then
+        printf " \n"
+        return
+    fi
+
+    local state=$(echo "$status" | sed -n 's/^status //p')
+    local artist=$(echo "$status" | sed -n 's/^tag artist //p')
+    local title=$(echo "$status" | sed -n 's/^tag title //p')
+
+    [[ ${#title} -gt $MAX_LENGTH_TITLE ]] && title="${title:0:$MAX_LENGTH_TITLE}..."
+    [[ ${#artist} -gt $MAX_LENGTH_ARTIST ]] && artist="${artist:0:$MAX_LENGTH_ARTIST}..."
+
+    case "$state" in
+        playing) printf "   %s  -  %s\n" "$title" "$artist" ;;
+        paused)  printf "   %s  -  %s\n" "$title" "$artist" ;;
+        *)       printf "   Stopped\n" ;;
+    esac
+}
+
 function main() {
-	if  pgrep -x lofi > /dev/null; then
-		printf "󰋋   Lofi Radio"
-		exit 0
-	fi
-
-	local status=$(cmus-remote -Q 2>/dev/null)
-
-	if [[ -z $status ]]; then
-		printf " "
-		exit 0
-	fi
-
-	local state=$(echo "$status" | sed -n 's/^status //p')
-	local artist=$(echo "$status" | sed -n 's/^tag artist //p')
-	local title=$(echo "$status" | sed -n 's/^tag title //p')
-
-	[[ ${#title} -gt $MAX_LENGTH_TITLE ]] && title="${title:0:$MAX_LENGTH_TITLE}..."
-	[[ ${#artist} -gt $MAX_LENGTH_ARTIST ]] && artist="${artist:0:$MAX_LENGTH_ARTIST}..."
-
-	case "$state" in
-		playing) printf "   %s  -  %s " "$title" "$artist" ;;
-		paused)  printf "   %s  -  %s " "$title" "$artist" ;;
-		*)       printf "   Stopped" ;;
-	esac
-
-	return 0
+    while true; do
+        getStatus
+        sleep 1
+    done
 }
 
 main "$@"
